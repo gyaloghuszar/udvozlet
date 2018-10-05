7d70953fe30846e8ad35e45c452fe04d3b2d2a3e
Dávid <david.676@hotmail.com>
2018-10-05 16:18:24 +0200

    Első változat. Egy osztály.

---
+++ src/udvozlet/Udvozlet.java
@@ -0,0 +1,17 @@
+
+package udvozlet;
+
+/**
+ *
+ * @author t1
+ */
+public class Udvozlet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Üdvözöllek!");
+    }
+    
+}
