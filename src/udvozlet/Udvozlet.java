365c66e739b240604440a0689f789833c416b25a
gyaloghuszar <43143924+gyaloghuszar@users.noreply.github.com>
2018-10-05 16:46:00 +0200

    itt van marvel kapitány

--- src/udvozlet/Udvozlet.java
+++ src/udvozlet/Udvozlet.java
@@ -11,7 +11,7 @@ public class Udvozlet {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Ember no=new Ember("Cecca");
+        Ember no=new Ember("Captain Marvel");
         Ember ferfi=new Ember("Benyaboyy");
         
         System.out.println(no.koszont());
